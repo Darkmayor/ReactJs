95faa6133991859937dd3383f8be24a2c378dc2d
Darkmayor <sanketkalokhe123@gmail.com>
2024-02-29 18:53:24 +0530

    Updating projects and finalizing them

--- CustomReact/customreact.js
+++ CustomReact/customreact.js
@@ -1,13 +1,24 @@
 const root = document.querySelector("#root");
 function Render(root , reactEle){
     // create a new attribute
+    // const DOMelem = document.createElement(reactEle.type);
+    // DOMelem.innerText = reactEle.children;
+    // //set href and target attribute of the reactele
+    // DOMelem.setAttribute('href' , reactEle.props.href);
+    // DOMelem.setAttribute('target' , reactEle.props.target);
+    //adding the component into root
+    // root.appendChild(DOMelem);
+
+    // the above way is good if we know how many attributes we have but what if we don't know how many attributes are there in the received object so to solve that we can iterate over the props
+
+    // create a new attribute
     const DOMelem = document.createElement(reactEle.type);
     DOMelem.innerText = reactEle.children;
-    //set href and target attribute of the reactele
-    DOMelem.setAttribute('href' , reactEle.props.href);
-    DOMelem.setAttribute('target' , reactEle.props.target);
-    //adding the component into root
+    for (const prop in reactEle.props) {
+            DOMelem.setAttribute(prop , reactEle.props[prop]);
+    }
     root.appendChild(DOMelem);
+
 
 }
 
