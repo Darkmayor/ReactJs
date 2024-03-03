740c4b6bd6badb06f97bcb9b67b58a3d4639b17a
Darkmayor <sanketkalokhe123@gmail.com>
2024-03-03 10:29:46 +0530

    Implemented usecallback hook , created passwordGemerator, created basic ui

--- Password-Generator/src/App.jsx
+++ Password-Generator/src/App.jsx
@@ -1,31 +1,56 @@
-import { useState } from 'react'
+import { useState , useCallback} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [length , setLength] = useState(8);
+  const [isNumberAllowed , setNumbers] = useState(false);
+  const [isCharacterAllowed , setCharacters] = useState(false);
+  const [password , setPassword] = useState(""); 
+
+  //create function for generating password
+
+  const PasswordGenerator = useCallback( () => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(isNumberAllowed) {
+      str+= "0123456789";
+    }
+    if(isCharacterAllowed){
+      str+= "!@#$%^&*-_+=~"
+    }
+
+    for (let i = 1; i <= length; i++) {
+      //select random text according to length
+      let char  = Math.floor(Math.random() * str.length + 1);
+      //append the generated text into pass
+      pass+= char;
+    }
+
+    setPassword(pass);
+
+  }, [length , isNumberAllowed , isCharacterAllowed , setPassword])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-3xl px-4 py-3 my-8 bg-gray-800 text-orange-500'> 
+      
+      <h1 className='text-white text-center my-3'> Password generator </h1>
+      
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            
+        />
+        <button
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
